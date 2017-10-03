4696dd8c8b741ccb980285ec4d295d2ac3f6de58
ChristianXCV <31867287+ChristianXCV@users.noreply.github.com>
2017-10-03 14:48:05 -0400

    Gamebaord

---
+++ Assets/Scripts/Basic3D.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basic3D : MonoBehaviour {
+
+	public GameObject CUBO;
+	public Vector3 movementAmount;
+	Vector3 restart;
+
+	// Use this for initialization
+	void Start () {
+		movementAmount = new Vector3 (1, -3, -1);
+
+		restart = CUBO.transform.position;
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.D)) {
+			CUBO.transform.position += new Vector3 (1, 0, 0);
+		}
+		if (Input.GetKeyDown (KeyCode.A)) {
+			CUBO.transform.position += new Vector3 (-1, 0, 0);
+
+		}
+		if (Input.GetKeyDown (KeyCode.W)) {
+			CUBO.transform.position += new Vector3 (0, 1, 0);
+		}
+		if (Input.GetKeyDown (KeyCode.S)) {
+			CUBO.transform.position += new Vector3 (0, -1, 0);
+		}
+
+
+
+	}
+}

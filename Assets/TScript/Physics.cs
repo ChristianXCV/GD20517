67adc35b5506d3d8fc342cf1c7ab91e4557e3163
ChristianXCV <31867287+ChristianXCV@users.noreply.github.com>
2017-10-10 00:20:37 -0400

    DogeHW

---
+++ Assets/TScript/Physics.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics : MonoBehaviour {
+
+    Rigidbody rb;
+    public float Doge;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * Doge);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.back * Doge);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * Doge);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * Doge);
+        }
+    }
+}
